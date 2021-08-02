2baee1015c71f360d5cc3005d0cd821912d53942
Tanner Dale <tanner@tannerdale.com>
2021-08-02 14:51:24 -0500

    Add iteration 3 tests

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -52,4 +52,60 @@ RSpec.describe Event do
       expect(event.food_trucks_that_sell(item4)).to eq([food_truck2])
     end
   end
+
+  context 'items sold' do
+    event = Event.new("South Pearl Street Farmers Market")
+
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    food_truck1.stock(item1, 35)
+    food_truck1.stock(item2, 7)
+
+    food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    food_truck2.stock(item4, 50)
+    food_truck2.stock(item3, 25)
+
+    food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    food_truck3.stock(item1, 65)
+    food_truck3.stock(item3, 10)
+
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+
+    it 'has a total inventory' do
+      expected = {
+        item1 => {
+          quantity: 100,
+          food_trucks: [food_truck1, food_truck3]
+        },
+        item2 => {
+          quantity: 7,
+          food_trucks: [food_truck1]
+        },
+        item4 => {
+          quantity: 50,
+          food_trucks: [food_truck2]
+        },
+        item3 => {
+          quantity: 35,
+          food_trucks: [food_truck2, food_truck3]
+        }
+      }
+
+      expect(event.total_inventory).to eq(expected)
+    end
+
+    it 'has overstocked items' do
+      expect(event.overstocked_items).to eq([item1])
+    end
+
+    it 'has an item list' do
+      expect(event.sorted_item_list).to eq(["Apple Pie (Slice)", "Banana Nice Cream", "Peach Pie (Slice)", "Peach-Raspberry Nice Cream"])
+    end
+  end
 end
